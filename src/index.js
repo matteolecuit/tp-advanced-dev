1d059091fa6dbacfd6c1a187622d09d1162b2b40
Adrien VRD <adrien.vaucard@gmail.com>
2022-03-04 09:30:53 +0100

    feat(global): calculation of totalprice

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import { taxRates } from "./res/taxRates.js";
+import * as readline from 'readline'
 console.log(taxRates);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let itemPrice = 0;
+let itemQuantity = 0;
+
+rl.question(`Quel est le prix de l'item ? `, price => {
+    itemPrice = price;
+    // if (typeof price === 'number') {
+        
+    // } else {
+    //     console.log("La valeur choisie doit être un nombre")
+    // }
+
+    rl.question(`Quelle quantité souhaitez vous acheter ? `, quantity => {
+        itemQuantity = quantity;
+            console.log("Le total sera de : " + itemQuantity * itemPrice)
+        // if (typeof(quantity) == 'number') {
+        // } else {
+        //     console.log("La valeur choisie doit être un nombre")
+        // }
+    })
+})
