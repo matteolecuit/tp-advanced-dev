40ab71d1bdf3ec7f245317c15d0183cb11db53ae
Adrien VRD <adrien.vaucard@gmail.com>
2022-03-04 11:07:40 +0100

    feat(global): some work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { taxRates } from "./res/taxRates.js";
+import { discountRates } from "./res/discountRates.js";
 import * as readline from 'readline'
 console.log(taxRates);
+console.log(discountRates);
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -11,6 +13,7 @@ let itemPrice = 0;
 let itemQuantity = 0;
 let discount = 0;
 let totalPrice = 0;
+let taxRate = 0;
 
 rl.question(`Quel est le prix de l'item ? `, price => {
     itemPrice = price;
@@ -20,18 +23,44 @@ rl.question(`Quel est le prix de l'item ? `, price => {
         totalPrice = itemQuantity * itemPrice
         console.log(`Le total HT sera de : ${totalPrice}`);
 
-        if (totalPrice > 1000) {
-            discount = 3;
-        }
-        console.log(`La réduction actuelle est de ${discount} %`);
+        rl.question(`Quel est votre code d'état ? `, stateCode => {
+            let foundTaxRate = taxRates.find(element => element.code == stateCode).rate;
 
-        rl.question(`Quelle est la valeur de la réduction à appliquer ? `, discount => {
-            discount = discount;
-        
+            if (foundTaxRate) {
+                taxRate = foundTaxRate;
+                console.log(`La TVA est de ${foundTaxRate} %`);
+            } else {
+                console.log(`Aucune code correspondant, le taux par défaut est de 20 %`);
+            }
+            
             console.log(`Le taux de réduction est de ${discount} %`);
             let ttcPrice = totalPrice * (1 - (discount / 100));
             console.log(`Le prix TTC est de ${ttcPrice}`);
-            rl.close()
+
+            if (totalPrice > 1000) {
+                discount = 3;
+            } else if (totalPrice > 5000) {
+                discount = 5;
+            } else if (totalPrice > 7000) {
+                discount = 7;
+            } else if (totalPrice > 10000) {
+                discount = 10;
+            } else if (totalPrice > 16000) {
+                discount = 15
+            }
+            console.log(`La réduction actuelle est de ${discount} %`);
+    
+            if (totalPrice <= 16000) {
+                rl.question(`Quelle est la valeur de la réduction à appliquer ? `, discount => {
+                    discount = discount;
+                
+                    console.log(`Le taux de réduction est de ${discount} %`);
+                    let ttcPrice = totalPrice * (1 - (discount / 100));
+                    console.log(`Le prix TTC est de ${ttcPrice}`);
+                    rl.close()
+                })
+                rl.close()
+            }
         })
     })
 })
