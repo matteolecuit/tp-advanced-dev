826c81c7424b2bd37b26a55686af2f0aebd6afbb
Matteo Lecuit <matteo@lecuit.fr>
2022-03-04 09:00:36 +0100

    add tax rates

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import { taxRates } from "./res/taxRates.js";
+console.log(taxRates);
---
+++ src/res/taxRates.js
@@ -0,0 +1,10 @@
+export const taxRates = [
+  {
+    code: "AZ",
+    rate: 0.2,
+  },
+  {
+    code: "VE",
+    rate: 0.12,
+  },
+];
